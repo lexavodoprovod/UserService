2a291ff27fe36764175b3ed8a0528314460cd6e7
lexavodoprovod <nikitoshololeenko18@gmail.com>
2026-03-10 15:30:55 +0300

    add db schema

---
+++ src/main/java/com/innowise/UserService/model/entity/PaymentCard.java
@@ -0,0 +1,36 @@
+package com.innowise.UserService.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payment_cards")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentCard extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 16)
+    private int number;
+
+    @Column(nullable = false)
+    private String holder;
+
+    @Column(name ="expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @Builder.Default
+    private boolean active = true;
+}
